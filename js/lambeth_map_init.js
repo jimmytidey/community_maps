566d37e439297f3cfd0bf683e19aa62121d23e3e
Jimmy Tidey <jimmytidey@gmail.com>
2013-11-16 16:15:55 +0000

    first commit

---
+++ js/lambeth_map_init.js
@@ -0,0 +1,11 @@
+//INITIALISE... 
+/*
+jQuery(document).ready(function () {
+    //Loop through all elements that instantiate a map
+    lambeth_maps = [];
+    jQuery('.lambeth_map').each(function (key, val) {
+        lambeth_maps.push(new lambeth_map(val));
+    });
+});
+*/
+
